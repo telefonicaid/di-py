ebe3b6615b510fafbafaeaa6e9695bb1d6f54c56
juandebravo <juandebravo@gmail.com>
2015-03-22 13:41:48 +0100

    fix multiprocessing issue in python

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+try:
+    # HACK: Avoid "TypeError: 'NoneType' object is not callable"
+    #      Related to issue http://bugs.python.org/issue15881
+    #      https://hg.python.org/cpython/rev/0a58fa8e9bac
+    import multiprocessing
+except ImportError:
+    pass
+
 setup(
     name='di-py',
     description='Dependency injection library',
